07fd0ed8a74a148bdcdccf8c3d18aaabbf815944
iklaq <iklaq.ahmad@jalantechnologies.com>
2023-03-23 20:52:49 +0530

    1. Created items data and stored it in databse, 2. Showed all the items to the user on website

--- routes/route.js
+++ routes/route.js
@@ -1,32 +1,68 @@
 const express = require('express');
 const router = express.Router();
-const categorie = require('../model/categories')
+const categorieDB = require('../model/categories')
+const itemDB = require('../model/items');
+
 
 
 // rendering homepage
-router.get('/',async (req,res)=>{
 
-    const categorieData =  await categorie.find()
+router.get('/',(req,res)=>{
+
+    res.render('homepage');
+})
+
+//rendering categories
+router.get('/categories',async (req,res)=>{
+
+    const categorieData =  await categorieDB.find()
     
-    res.render('homepage',{cat:categorieData});
+    res.render('categories',{cat:categorieData});
     
 })
 
+//rendering items
+router.get('/items',async(req,res)=>{
 
-// adding data in categorie from user in database
+  const itemsData =  await itemDB.find()
+  const categorieData =  await categorieDB.find()
+  
+  res.render('items',{allCategories:categorieData,allItems:itemsData});
+})
+
+
+// Creating data in categorie collection from user in database
 router.post("/add/categorie",async (req,res)=>{
 
     const categorieData = req.body
+    categorieData.numberOfItems = 0;
     console.log(categorieData);
 
-    const data = new categorie(categorieData);
+    const data = new categorieDB(categorieData);
     await data.save()
     .then(() => {
-      console.log("data stored successfully");
-      res.redirect("/");
+      console.log("categorie data stored successfully");
+      res.redirect("/categories");
     })
     .catch(() => {
-      console.log("data does not stored");
+      console.log("categorie data does not stored");
+    });
+   
+})
+
+// Creating data in items collection from user in database
+router.post("/add/item", async(req,res)=>{
+   const itemData = req.body
+    console.log(itemData);
+
+    const data = new itemDB(itemData);
+    await data.save()
+    .then(() => {
+      console.log(" items data stored successfully");
+      res.redirect('/items')
+    })
+    .catch(() => {
+      console.log("items data does not stored");
     });
    
 })
