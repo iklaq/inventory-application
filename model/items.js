b12acfcbc2ef83b92b8cba29fcc8b4db80026537
iklaq <iklaq.ahmad@jalantechnologies.com>
2023-03-23 16:17:10 +0530

    completed create categorie section

---
+++ model/items.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+
+    itemName: String,
+    itemPrice: Number,
+    itemCategorie: String
+});
+
+module.exports = mongoose.model("items",itemSchema);
+
